1bac8ecd54926e63a064436eb06f4c90f2d452af
Shane Wade <shanebramble96@hotmail.com>
2024-10-28 19:15:07 -0500

    implemented mock data for potion constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,9 +1,24 @@
 import { Player } from "../lib/Player";
-
-test ("creates a player object", () => {
-const player = new Player ("Dave");
-expect(player.name).toBe("Dave");
-expect(player.health).toEqual(expect.any(Number));
-expect(player.strength).toEqual(expect.any(Number));
-expect(player.agility).toEqual(expect.any(Number));
+import { Potion } from "../lib/Potion";
+// Then jest.mock() mocks/replaces the constructor's implementation with our faked data.
+jest.mock("../lib/Potion.js", () => {
+  return {
+    Potion: jest.fn().mockImplementation(() => ({
+      name: "health",
+      value: 20,
+    })),
+  };
 });
+
+test("creates a player object", () => {
+  const player = new Player("Dave");
+  expect(player.name).toBe("Dave");
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
+});
+
+console.log(new Potion());
